937b77185e2a2c5623351b45601653d290a756c4
nitinkgp23 <nitin.iitkgp23@gmail.com>
2016-06-12 13:01:12 +0530

    Initial commit

---
+++ api.js
@@ -0,0 +1,20 @@
+// Here we run a very simple test of the Graph API after login is
+  // successful.  See statusChangeCallback() for when this call is made.
+  function testAPI() {
+    console.log('Welcome!  Fetching your information.... ');
+    FB.api('/me', {fields: "id,birthday,email,gender,location,name"}, function(response) {
+      console.log('Successful login for: ' + response.name);
+      console.log(response);
+      var location = response.location;
+      document.getElementById('status').innerHTML =
+        'Thanks for logging in, ' + response.name + '!' + '<br />' +'<TABLE><TR><TD> Id </TD><TD>'+response.id+'</TD></TR><TR> <TD> Birthday </TD> <TD>'+response.birthday+'</TD> </TR><TR> <TD> Email </TD> <TD>'+response.email+'</TD> </TR> <TR> <TD> Gender </TD> <TD>'+response.gender+'</TD> </TR></TABLE> <br />';
+
+        FB.api('/' + location.id, {
+            fields: 'location'
+        }, function(locationResponse) {
+            console.log(locationResponse); //will print your desired location object
+        }); 
+
+      });
+
+    }
